056bda399c0a7bdfefefb8fc2dc8db0e28289a8a
MLamothe <maxime.lamothe@polymtl.ca>
2022-09-30 10:55:37 -0400

    Bugfix pour 3 modifications/bugs + tests pour les bugs

--- src/test_CCoinBox.py
+++ src/test_CCoinBox.py
@@ -10,3 +10,16 @@ class Test_CCoinBox(unittest.TestCase):
         monnaie = self.coin_box.get_monnaie_courante()
         self.coin_box.ajouter_25c()
         self.assertEqual(self.coin_box.get_monnaie_courante(), monnaie + 1)
+
+    def test_ajouter_25c_get_vente_permise(self):
+        self.assertFalse(self.coin_box.get_vente_permise())
+
+    def test_apres_vente_avec_2_monnaie_courante_vente_non_permise(self):
+        self.coin_box.ajouter_25c()
+        self.coin_box.ajouter_25c()
+        self.coin_box.vente()
+        self.assertFalse(self.coin_box.get_vente_permise())
+
+    def test_vente_non_permise_apres_insertion_1_monnaie(self):
+        self.coin_box.ajouter_25c()
+        self.assertFalse(self.coin_box.get_vente_permise())
