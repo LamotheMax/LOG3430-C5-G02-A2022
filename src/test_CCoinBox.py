4589a2e969a32f1e7fb21fb194257ba242f6d0bc
MLamothe <maxime.lamothe@polymtl.ca>
2022-09-30 10:09:19 -0400

    BugFix pour ligne 10 + test pour ligne 10

--- src/test_CCoinBox.py
+++ src/test_CCoinBox.py
@@ -3,6 +3,10 @@ from CCoinBox import CCoinBox
 
 class Test_CCoinBox(unittest.TestCase):
 
-    def test_something(self):
-        self.assertTrue(True)
+    def setUp(self):
+        self.coin_box = CCoinBox()
 
+    def test_ajouter_25_c(self):
+        monnaie = self.coin_box.get_monnaie_courante()
+        self.coin_box.ajouter_25c()
+        self.assertEqual(self.coin_box.get_monnaie_courante(), monnaie + 1)
